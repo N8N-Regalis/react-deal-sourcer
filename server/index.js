50aacc6bc5da53eda589fd8058f8a1dbcdafdff4
Raldz <shredraldz@gmail.com>
2026-05-19 05:44:55 +0800

    Temporarily allow all CORS origins for debugging

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-  origin: ['https://regalis-deal-sourcer.netlify.app', 'http://localhost:5173', 'http://localhost:3000', 'http://localhost:5000']
+  origin: true,
+  credentials: true
 }))
 app.use(express.json())
 
